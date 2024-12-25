2b4100a09642e83c93aaf57a65b032b2eb572981
Upgrader Boy <upgraderboy@gmail.com>
2024-12-25 11:01:36 +0530

    bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,23 @@
 import React from 'react';
 import { useBarcode } from 'next-barcode';
 import Image from 'next/image';
+import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import Link from 'next/link';
+import { Button, buttonVariants } from '@/components/ui/button';
 
 function App() {
-  const { inputRef } = useBarcode({
-    value: 'next-barcode',
-    options: {
-      background: '#ffff00',
-    }
-  });
 
-  return <Image fill alt="Bar Code" ref={inputRef} />;
+  return (
+    <MaxWidthWrapper className="py-20 mx-auto text-center flex flex-col items-center max-w-3xl">
+      <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Your Own for high-quality{" "}<span className={"text-primary"}>Invoice Generator</span>.</h1>
+    <p className="mt-6 text-lg max-w-prose text-muted-foreground">Welcome to InVoice Hub. Every item on our platform is verified by our team to ensure our highest quality standards.</p>
+    <div className="flex flex-col sm:flex-row gap-4 mt-6">
+      <Link href="/products" className={buttonVariants()}>Browse Trending</Link>
+      <Button variant={"ghost"}>Our quality promise &rarr;</Button>
+    </div>
+    {/* TODO: List products */}
+    </MaxWidthWrapper>
+  )
 };
 
 export default App;
